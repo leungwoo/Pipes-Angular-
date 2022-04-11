d9808615db8eb42f97643ec3746f14dcb5159002
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-04-10 20:05:57 -0400

    case date currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  public name!: string;
+  public date!: string;
+  public amount!: number;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
